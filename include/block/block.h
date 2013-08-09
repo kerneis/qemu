05b1bcce5d402a0db6452f0b8d51f45b1ca8142c
Charlie Shepherd <charlie@ctshepherd.com>
2013-08-09 17:22:48 +0200

    Make bdrv_discard coroutine only and add bdrv_sync_discard
    This patch renames the coroutine only bdrv_co_discard to bdrv_discard and the original
    bdrv_discard to bdrv_sync_discard. bdrv_sync_discard is synchronous only.

--- include/block/block.h
+++ include/block/block.h
@@ -282,8 +282,9 @@ int bdrv_flush_all(void);
 void bdrv_close_all(void);
 void bdrv_drain_all(void);
 
-int bdrv_discard(BlockDriverState *bs, int64_t sector_num, int nb_sectors);
-int bdrv_co_discard(BlockDriverState *bs, int64_t sector_num, int nb_sectors);
+int coroutine_fn bdrv_discard(BlockDriverState *bs, int64_t sector_num,
+                              int nb_sectors);
+int bdrv_sync_discard(BlockDriverState *bs, int64_t sector_num, int nb_sectors);
 int bdrv_has_zero_init_1(BlockDriverState *bs);
 int bdrv_has_zero_init(BlockDriverState *bs);
 int bdrv_is_allocated(BlockDriverState *bs, int64_t sector_num, int nb_sectors,
