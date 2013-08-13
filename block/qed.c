9dee76de631eb2c759e43f2879cfc111b7dac0da
Charlie Shepherd <charlie@ctshepherd.com>
2013-08-13 23:12:49 +0200

    Fix bdrv_qed_open -> bdrv_qed_co_open

--- block/qed.c
+++ block/qed.c
@@ -1526,7 +1526,7 @@ static void bdrv_qed_invalidate_cache(BlockDriverState *bs)
 
     bdrv_qed_close(bs);
     memset(s, 0, sizeof(BDRVQEDState));
-    bdrv_qed_open(bs, NULL, bs->open_flags);
+    bdrv_qed_co_open(bs, NULL, bs->open_flags);
 }
 
 static int bdrv_qed_check(BlockDriverState *bs, BdrvCheckResult *result,
